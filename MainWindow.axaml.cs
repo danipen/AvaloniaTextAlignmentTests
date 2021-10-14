79c531fc32e0c075b6a0d245af778725ba9fcfc7
Daniel <dpenalba@codicesoftware.com>
2021-10-14 11:44:26 +0200

    Test both textlayout and formatted text

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
+using Avalonia.Media.TextFormatting;
 
 namespace AvaloniaTextAlignmentTests
 {
@@ -28,12 +29,18 @@ namespace AvaloniaTextAlignmentTests
                 base.Render(context);
 
                 FormattedText formattedText = new FormattedText(
-                    "| This is a sample text just to check the alignment 0123456789 |",
+                    "FormattedText -> | This is a sample text just to check the alignment 0123456789 |",
                     new Typeface("Inter"),
-                    12,
+                    24,
                     TextAlignment.Left,
                     TextWrapping.NoWrap,
                     Size.Infinity);
+
+                TextLayout textLayout = new TextLayout(
+                    "TextLayout -> | This is a sample text just to check the alignment 0123456789 |",
+                    new Typeface("Inter"),
+                    24,
+                    Brushes.Black);
 
                 context.DrawText(
                     Brushes.Black,
@@ -43,6 +50,15 @@ namespace AvaloniaTextAlignmentTests
                 context.DrawRectangle(
                     new Pen(Brushes.Red, 1),
                     new Rect(10, 10, formattedText.Bounds.Width, formattedText.Bounds.Height));
+
+                using (context.PushPreTransform(Avalonia.Matrix.CreateTranslation(new Vector(10, 50))))
+                {
+                    textLayout.Draw(context);
+                }
+
+                context.DrawRectangle(
+                    new Pen(Brushes.Red, 1),
+                    new Rect(10, 50, textLayout.Size.Width, textLayout.Size.Height));
             }
         }
     }
